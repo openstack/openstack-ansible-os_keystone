2dda74a3d04edea9560fc288f931c81a03db83e8
Jonathan Rosser <jonathan.rosser@rd.bbc.co.uk>
2020-10-01 16:47:35 +0100

    Fix linter errors
    Add noqa where requried http://paste.openstack.org/show/798685/
    
    Also remove centos-7 jobs as these are not supported for Victoria and
    are blocking further patches merging.
    Change-Id: I92f9131daf23f5cd38054a0b7c385344994a2aa0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -240,7 +240,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'doc-' + target_name + '.tex',
-     title.replace("_", "\_"), author, 'manual'),
+     title.replace("_", r"\_"), author, 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
